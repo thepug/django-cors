96dc3c8ff4b1938d958d2745d9eb2d8c7586f853
Nathan Zorn <nathan.zorn@gmail.com>
2011-10-04 09:00:31 -0400

    add the Access-Control-Allow-Credentials and Access-Control-Expose-Headers to the response.

--- cors.py
+++ cors.py
@@ -47,5 +47,7 @@ class CORSMiddleware(object):
 					response['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
 					response['Access-Control-Max-Age'] = 1000
 					response['Access-Control-Allow-Headers'] = '*'
+					response['Access-Control-Allow-Credentials'] = "true"
+					response['Access-Control-Expose-Headers'] = '*'
 				break
 		return response
